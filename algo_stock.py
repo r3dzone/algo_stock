f2125c2076e0677d8a27d28ab7434f2669dcc3df
R3dzone <coolitop123@gmail.com>
2021-03-06 22:33:11 +0900

    add account info event handler

--- algo_stock.py
+++ algo_stock.py
@@ -8,17 +8,24 @@ class login:
     
     def OnLogin(self,code,msg):
         if code == "0000":
-            print("login OK")
+            print("login Success:"+pw[0])
             login.login_state = 1
         else:
-            print("login failure")
+            print("login failure code:"+code+" msg:"+msg)
+            
+class XAQueryHandler_T1102:
+    query_state = 0
+    
+    def OnReciveData(self,code):
+        XAQueryHandler_T1102.query_state = 1
+        
 
-pw_file = open('C:/Users/unknown/Desktop/stock_passwd.txt', 'r')
+pw_file = open('C:/Users/R3dzone/Desktop/stock_passwd.txt', 'r')
 pw = []
 for i in pw_file.readlines():
     pw.append(i.rstrip("\n"))
 
-id = pw[0]
+id = pw[0] 
 pswd = pw[1]
 cert_pswd = pw[2] 
 
@@ -28,3 +35,11 @@ initXASession.Login(id,pswd,cert_pswd,0,0)
 
 while login.login_state == 0:
     pythoncom.PumpWaitingMessages()
+    
+account_num = initXASession.GetAccountListCount()
+print("계좌 수:"+str(account_num))
+
+for i in range(account_num):
+    account = initXASession.GetAccountList(i)
+    print("계좌정보:"+account)
+
